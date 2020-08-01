83d511596851148bcd5e7f63d831dbe04cfa3555
Neeraj Yadav <ny29599@gmail.com>
2020-08-01 11:47:44 +0530

    added basic setup
    connected with mongo db

--- server/index.js
+++ server/index.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const {MONGOURI} = require('./keys')
 const app = express();
 const PORT = 5000;
+
+mongoose.connect(MONGOURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+mongoose.connection.on('connected', () => {
+    console.log('mongo db connected')
+})
+mongoose.connection.on('error', (err) => {
+    console.log('mongo db error occured', err)
+})
 
 app.get('/', (req, res) => {
     res.send('hello')
