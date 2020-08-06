240b45fe6b2501834ebd29702770c19d25b763f1
Neeraj Yadav <ny29599@gmail.com>
2020-08-06 10:43:23 +0530

    UI build
    Signup and Login screen build with materialize

--- client/src/components/screens/Login.js
+++ client/src/components/screens/Login.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Login= () => {
     return(
-        <h1>Login</h1>
+        <div className="mycard">
+            <div className="card auth-card">
+                <h2>Stoned-Gram</h2>
+                <input type="email" placeholder="email" />
+                <input type="password" placeholder="password" />
+                <button className="btn waves-effect waves-light">
+                    Login
+                </button>
+                <h5>
+                    <Link to="/signup" >Don't have an account?</Link>
+                </h5>
+            </div>
+        </div>
     )
 }
 export default Login;
--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
-const Signup= () => {
+const Login= () => {
     return(
-        <h1>Signup</h1>
+        <div className="mycard">
+            <div className="card auth-card">
+                <h2>Stoned-Gram</h2>
+                <input type="text" placeholder="name" />
+                <input type="email" placeholder="email" />
+                <input type="password" placeholder="password" />
+                <button className="btn waves-effect waves-light">
+                    Login
+                </button>
+                <h5>
+                    <Link to="/login" >Already have an account?</Link>
+                </h5>
+            </div>
+        </div>
     )
 }
-export default Signup;
+export default Login;
