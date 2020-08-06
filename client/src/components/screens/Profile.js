ce806cfbfcc9edae20346d0f09ac8d760935affc
Neeraj Yadav <ny29599@gmail.com>
2020-08-06 11:25:00 +0530

    profile added
    ui of profile screen added

--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -2,7 +2,38 @@ import React from 'react';
 
 const Profile= () => {
     return(
-        <h1>Profile</h1>
+       <div style={{
+           maxWidth: "550px",
+           margin: "0px auto"
+       }} >
+           <div style={{
+               display: "flex",
+               justifyContent: "space-around",
+               margin: "18px 0px",
+               borderBottom: "1px solid grey"
+           }}>
+               <div>
+                   <img style={{width: "160px", height:"160px", borderRadius:"80px"}}
+                   src="https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" />
+               </div>
+               <div>
+                   <h4>Ramesh Verma</h4>
+                   <div style={{display: "flex", width: "108%", justifyContent:"space-between"}} >
+                       <h6>30 posts</h6>
+                       <h6>20 followers</h6>
+                       <h6>12 following</h6>
+                   </div>
+               </div>
+           </div>
+           <div className="gallery">
+            <img className="item" src="https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" />
+            <img className="item" src="https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" />
+            <img className="item" src="https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" />
+            <img className="item" src="https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" />
+            <img className="item" src="https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" />
+            <img className="item" src="https://images.unsplash.com/photo-1569466896818-335b1bedfcce?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" />
+           </div>
+       </div>
     )
 }
 export default Profile;
