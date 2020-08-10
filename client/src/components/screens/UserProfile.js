0201545fc69d6adb65838a70bff631b0fe91aca9
Neeraj Yadav <ny29599@gmail.com>
2020-08-10 12:43:52 +0530

    follow unfollow concept
    implemented the logic and front end for following and followers

--- client/src/components/screens/UserProfile.js
+++ client/src/components/screens/UserProfile.js
@@ -4,6 +4,7 @@ import {useParams} from 'react-router-dom'
 
 const Profile= () => {
     const [userProfile, setProfile] = useState(null)
+    const [showFollow, setShowFollow] = useState(true)
     const {state, dispatch} = useContext(UserContext)
     const {userid} = useParams()
     useEffect(() => {
@@ -16,6 +17,59 @@ const Profile= () => {
             setProfile(result)
         })
     }, [])
+    const followUser = () => {
+        fetch('/follow', {
+            method: 'put',
+            headers: {
+                "Content-type": 'application/json',
+                "Authorization": 'Bearer ' + localStorage.getItem('jwt')
+            },
+            body: JSON.stringify({
+                followId: userid
+            })
+        }).then(res => res.json())
+        .then(data => {
+            dispatch({type: 'UPDATE', payload:{following: data.following, followers: data.followers}})
+            localStorage.setItem('user', JSON.stringify(data))
+            setProfile((prevState) => {
+                return {
+                    ...prevState,
+                    user:{
+                        ...prevState.user,
+                        followers:[...prevState.user.followers,data._id]
+                    }
+                }
+            })
+            setShowFollow(false)
+        })
+    }
+    const unfollowUser = () => {
+        fetch('/unfollow', {
+            method: 'put',
+            headers: {
+                "Content-type": 'application/json',
+                "Authorization": 'Bearer ' + localStorage.getItem('jwt')
+            },
+            body: JSON.stringify({
+                unfollowId: userid
+            })
+        }).then(res => res.json())
+        .then(data => {
+            dispatch({type: 'UPDATE', payload:{following: data.following, followers: data.followers}})
+            localStorage.setItem('user', JSON.stringify(data))
+            setProfile((prevState) => {
+                const newFollower = prevState.user.followers.filter(item=>item != data._id )
+                return {
+                    ...prevState,
+                    user:{
+                        ...prevState.user,
+                        followers:newFollower
+                    }
+                }
+            })
+            setShowFollow(true)
+        })
+    }
     return(
         <>
             {
@@ -39,9 +93,20 @@ const Profile= () => {
                                 <h4>{userProfile.user.email}</h4>
                                 <div style={{display: "flex", width: "108%", justifyContent:"space-between"}} >
                                     <h6>{userProfile.posts.length} posts</h6>
-                                    <h6>20 followers</h6>
-                                    <h6>12 following</h6>
+                                    <h6>{userProfile.user.followers.length} followers</h6>
+                                    <h6>{userProfile.user.following.length} following</h6>
                                 </div>
+                                {
+                                    showFollow
+                                    ?
+                                    <button style={{margin: '10px'}} className="btn waves-effect waves-light" onClick={() => followUser()} >
+                                        follow
+                                    </button>
+                                    :
+                                    <button style={{margin: '10px'}} className="btn waves-effect waves-light" onClick={() => unfollowUser()} >
+                                        unfollow
+                                    </button>
+                                }
                             </div>
                         </div>
                         <div className="gallery">
